482d92bae0513562546f8ccb372a7427f8f29397
lancerael-undo <122271901+lancerael-undo@users.noreply.github.com>
2023-05-29 11:16:46 +0100

    fixed import

--- src/components/BarChart/BarChart.ts
+++ src/components/BarChart/BarChart.ts
@@ -1,6 +1,6 @@
 import { easeLinear } from 'd3-ease'
 import { select } from 'd3-selection'
-import transition from 'd3-transition'
+import * as transition from 'd3-transition'
 import AxisChart from '../AxisChart'
 import { BarChartConfig, ChartParams, ConfigItemValue } from '../../types'
 
--- src/components/LineChart/LineChart.ts
+++ src/components/LineChart/LineChart.ts
@@ -1,7 +1,7 @@
 import { line } from 'd3-shape'
 import { select } from 'd3-selection'
 import { scalePoint } from 'd3-scale'
-import transition from 'd3-transition'
+import * as transition from 'd3-transition'
 import AxisChart from '../AxisChart'
 import { AxisChartConfig, ChartParams } from '../../types'
 import { easeLinear } from 'd3-ease'
